622e0485718373d51de93d49ac55c121c53b47c3
S9963hd <76931140+S9963hd@users.noreply.github.com>
2024-07-15 18:07:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Favourite from './FavouritePage/Favourite';
 // Login Page
 import Login from './LoginPage/Login';
 import SignUp from './LoginPage/SignUp';
-import {auth,getCookie} from './LoginPage/Login';
+import {auth} from './LoginPage/Login';
 // Song Context
 export const songContext = createContext();
 
@@ -21,7 +21,7 @@ function App() {
   const [songs, setSongs] = useState([]);
   const [index, setIndex] = useState(null);
   const [playStack, setPlayStack] = useState({});
-  const [login,setLogin]=useState(JSON.parse(localStorage.getItem('myObject')));
+  const [login,setLogin]=useState(auth());
   const[favourites,setFavourites]=useState([]);
   const[songsFlag,setSongsFlag]=useState([]);
   // next Song
