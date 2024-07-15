222dfe3125b2f88bd11b620595e5a59d38918577
S9963hd <76931140+S9963hd@users.noreply.github.com>
2024-07-15 20:35:16 +0530

    Update Login.jsx

--- src/LoginPage/Login.jsx
+++ src/LoginPage/Login.jsx
@@ -13,7 +13,7 @@ export function auth() {
         try {
             const user = JSON.parse(userCookie);
             console.log("Cookie Gotcha :)");
-            return user;
+            return user.email;
         } catch (e) {
             console.error("Error parsing cookie:", e);
             return null;
