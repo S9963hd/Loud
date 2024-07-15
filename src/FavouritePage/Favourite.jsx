3cf9431603501b0ddd26c8271bd30ab738e0d295
S9963hd <76931140+S9963hd@users.noreply.github.com>
2024-07-15 21:07:23 +0530

    Update Favourite.jsx

--- src/FavouritePage/Favourite.jsx
+++ src/FavouritePage/Favourite.jsx
@@ -13,7 +13,7 @@ const Favourite = () => {
         await axios({
           method:"POST",
           url:"https://loudbackendfavourites.onrender.com/getfavourites",
-          data:login
+          data:login.email
         }).then(res=>setFavourites(res.data)).catch(err=>{console.log("Server Error");setInterval(()=>fetchFavourites(),2000)})
         console.log("Favourites fetched successfully", favourites)
     };
