a619c3cc20de1f8c597dc544bda5671722037af8
Sanjay <sanjaymajstic143@gmail.com>
2024-07-16 14:45:48 +0530

    final-3

--- src/FavouritePage/Favourite.jsx
+++ src/FavouritePage/Favourite.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect,useState } from 'react';
 import { Card } from '../SearchPage/Search';
 import './Favourite.css';
 import axios from 'axios'; 
@@ -6,7 +6,7 @@ import { songContext } from '../App';
 
 const Favourite = () => {
   const { favourites, login, setFavourites, setSongs } = useContext(songContext);
-
+  const[loader,setLoader]=useState(login!=null);
   const fetchFavourites = async () => {
     try {
       const response = await axios.post("https://loudbackendfavourites.onrender.com/getfavourites", {
@@ -33,7 +33,7 @@ const Favourite = () => {
           </span>
         ))
       ) : (
-        <h1>No Songs Found</h1>
+        (login)?<h1 className={loader ? "fa-solid fa-spinner loader" : "hide"} style={{width:'99vw',textAlign:'center',fontSize:'xx-large',color:'white'}}></h1>:<h1>No Songs Found</h1>
       )}
     </div>
   );
